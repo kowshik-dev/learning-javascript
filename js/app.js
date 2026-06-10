5105a2f73da962981e89b9507d4525026186087f
Ikram-Hossain <ikramhossain.dev@gmail.com>
2026-06-10 16:30:06 +0600

    add JavaScript examples to app.js and update title in index.html

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+// Examples of integers
+console.log(3);
+console.log(5);
+console.log(-67);
+
+// Examples of floating point numbers
+console.log(3.14);
+console.log(7.2);
+console.log(-14.5);
+
+// Example of a string using double quotes
+console.log("I love to code!");
+
+// Example of a string using single quotes
+console.log('JavaScript is fun!');
+
+// Example of a boolean value
+console.log(true);
+console.log(false);
+
+// Example of a null value
+console.log(null);
+
+// Example of an undefined value
+console.log(undefined);
+
+// Example of typeof operator
+console.log(typeof 42); // number
+console.log(typeof 3.14); // number
+console.log(typeof "hello"); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object (this is a known quirk in JavaScript)
+console.log(typeof undefined); // undefined
+
+const person = {
+  name: "Alice",
+  age: 30
+};
+console.log(person);
+
