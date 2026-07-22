59e89fb6d98b683a7ef0e60310d3523f4506354a
Ikram-Hossain <ikramhossain.dev@gmail.com>
2026-07-22 20:39:43 +0600

    loop

---
+++ js/10-loop/01-loop.js
@@ -0,0 +1,6 @@
+//for loop
+//A for loop repeats code while a condition stays true.
+
+for (let ticketNumberL = 1; ticketNumberL <= 5; ticketNumberL++) {
+  console.log(`Ticket ${ticketNumberL}`);
+}
