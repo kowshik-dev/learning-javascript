58a4f05774108970bc4ecb1d80dd43c66e200e17
Ikram-Hossain <ikramhossain.dev@gmail.com>
2026-08-02 01:46:11 +0600

    async

--- js/11-async/11-async.js
+++ js/11-async/11-async.js
@@ -42,6 +42,160 @@
 // End
 // Timer finished
 
+//Task 1
+async function getPost(){                 //async function always returns a Promise.
+
+    const response = await fetch(        //wait until this Promise finishes, then give me the result.
+        "https://jsonplaceholder.typicode.com/posts/1"
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+}
+
+getPost();
+
+// before:
+//             fetch()
+//              ↓
+//            .then(response)
+
+//now: 
+//            await fetch()
+//              ↓
+//            response
+
+
+//Task 2
+async function getUsrData() {          //async function return a promise
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users/1");  //wait untill promise finished
+    const data = await response.json();
+    console.log(data);
+
+    } catch(error){
+        console.log(error);
+    }
+}
+getUsrData()
+
+
+// Task 3
+async function getPost() {
+
+    try{
+
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/5")
+
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+getPost()
+
+
+
+// / //Task 4
+// async function getPost() {
+
+//     try{
+
+//         const response = await fetch("https://jsonplaceholder.typicode.com/todos/7")
+
+//         const data = await response.json();
+
+//         console.log(data.title);
+
+//     } catch(error){
+
+//         console.log(error);
+
+//     }
+
+// }
+
+// getPost()
+
+
+
+// //Task 5
+// async function getPost() {
+
+//     try{
+
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users/3")
+
+//         const data = await response.json();
+
+//         console.log(data.name);
+
+//         console.log(data.email);
+
+//         console.log(data.address.city)
+
+//     } catch(error){
+
+//         console.log(error);
+
+//     }
+
+// }
+
+getPost()
+
+
+//Task 6
+// async function getUser(){
+
+//     console.log("Loading...");
+
+//     try{
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users/8")
+//         const data = await response.json();
+//         console.log("Loaded successfully!")
+//         console.log(data.username)
+//         console.log(data.phone)
+//         console.log(data.website)
+//     } catch(error){
+//         console.log(error)
+//     };
+// }
+// getUser()
+
+
+// 200 OK
+// 201 Created
+// 202 Accepted
+// 204 No Content
+
+// 400 Bad Request
+// 401 Unauthorized
+// 403 Forbidden
+// 404 Not Found
+// 500 Internal Server Error
+
+
+// async function test() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+
+//     console.log(response);
+//     console.log(response.ok);
+//     console.log(response.status);
+//     console.log(response.statusText);
+// }
+
+// test();
+
 
 
 
---
+++ js/11-async/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+try{
+
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users/3")
+
+//         const data = await response.json();
+
+//         console.log(data.name);
+
+//         console.log(data.email);
+
+//         console.log(data.address.city)
+
+//     } catch(error){
+
+//         console.log(error);
+
+//     }
+
+// }
